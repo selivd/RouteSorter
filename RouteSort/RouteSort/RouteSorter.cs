998aea417280039a87c29bce339bc5b3ade79360
Dmitriy Seliverstov <dseliverstov@platius.ru>
2016-07-22 09:28:54 +0300

    #3 Оценка сложности в комментариях

--- RouteSort/RouteSort/RouteSorter.cs
+++ RouteSort/RouteSort/RouteSorter.cs
@@ -21,8 +21,11 @@ namespace RouteSort
     {
         public static IList<IRoute<T>> Sort(IEnumerable<IRoute<T>> routes)
         {
+            // Сложность O(N) для наполнения словаря
             var connectionMap = BuildConnectionMap(routes);
+            // O(N) для наполнения списка, просмотра словаря
             var result = GetSortedRoutes(connectionMap);
+            // В результате сложность сортировки составила O(N)
             return result;
         }
 
@@ -30,21 +33,27 @@ namespace RouteSort
         {
             if (!connectionMap.Any())
                 return new List<IRoute<T>>();
+            // O(N) на просмотр словаря
             var startPoints = connectionMap.Where(a => a.Value.PreviousRoute == null).Take(2).ToArray();
             if (startPoints.Length == 0)
                 throw new CycleExistsException();
             if (startPoints.Length > 1)
                 throw new NotSingleRouteException();
+            // массив не более 2 элементов, пренебрегаем, o(1) 
             var startOfPath = startPoints.Single();
+            // Пренебрегая известной длиной коллекции, получаем 2*O(N) для наполнения списка из-за его перестроений.
             var result = new List<IRoute<T>>();
             var currentRoute = startOfPath.Value.NextRoute;
+            // O(1) на поиск в словаре N раз
             while (currentRoute != null)
             {
                 result.Add(currentRoute);
                 var endPoint = currentRoute.EndPoint;
                 currentRoute = connectionMap[endPoint].NextRoute;
+                // это бесплатно
                 connectionMap.Remove(endPoint);
             }
+            // Итого O(N) - наполнение списка, просмотр словаря
             if (connectionMap.Count > 1)
                 throw new NotSingleRouteException();
             return result;
@@ -52,7 +61,11 @@ namespace RouteSort
 
         private static Dictionary<T, PointDescription> BuildConnectionMap(IEnumerable<IRoute<T>> routes)
         {
+            // Из-за незнания длины входной коллекции будет несколько перестроений словаря, 
+            // поэтому, общее время заполнения словаря увеличится примерно в 2 раза до 2*O(N),
+            // где O(N) - наполнение словаря коллекцией с известной длиной и пренебреженной вероятностью коллизий хэш кодов элементов.
             var connectionsMap = new Dictionary<T, PointDescription>();
+
             foreach (var route in routes)
             {
                 PointDescription startPoint;
